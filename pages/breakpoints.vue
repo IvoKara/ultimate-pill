99ae79c7a7868477dc6bff6f7731f12c4e4786e0
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2022-12-23 18:01:20 +0200

    Update breakpoints

--- pages/breakpoints.vue
+++ pages/breakpoints.vue
@@ -7,21 +7,16 @@ definePageMeta({
 
 const breakpoints = useBreakpoints(designBreakpoints)
 
-const grid4 = breakpoints.smallerOrEqual('md')
+const grid4 = breakpoints.smaller('md')
 const grid8 = breakpoints.between('md', 'lg')
-const grid12 = breakpoints.greaterOrEqual('lg')
+const grid12 = breakpoints.greater('lg')
 </script>
 
 <template>
   <div bg="#FFB0BA">
     <main px-grid-br xxl="w-1920px mx-auto">
       <div
-        bg="#C7E2FB" w-full h-screen grid grid-rows-1 gap-4
-        :class="{
-          'grid-cols-4': grid4,
-          'grid-cols-8': grid8,
-          'grid-cols-12': grid12,
-        }"
+        bg="#C7E2FB" w-full h-screen grid gap-4 grid-rows-1 grid-cols-4 md:grid-cols-8 lg:grid-cols-12
       >
         <div v-for="_, i in grid4 ? 4 : grid8 ? 8 : 12" :key="i" bg="#8FBEEA" />
       </div>
