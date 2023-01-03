2b379c2db31f061bd9efcdbd5ff7c20cf22a9e3b
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2023-01-03 16:25:25 +0200

    Update shrink behavour of navbar items

--- components/Shop/ShopHeader.vue
+++ components/Shop/ShopHeader.vue
@@ -1,17 +1,17 @@
 <template>
   <header class="sticky left-0 right-0 top-0">
     <slot name="banner" />
-    <nav class="border-b-1 border-black backdrop-blur h-16 lg:(h-20 gap-20) flex items-center justify-between gap-4 px-grid-br py-2">
+    <nav class="border-b-1 border-black backdrop-blur h-16 lg:(h-20 gap-20) xxl:text-4.25 flex items-center justify-between gap-4 px-grid-br py-2">
       <div
         flex items-center justify-between gap-12 xl:w-210
       >
-        <div class="flex gap-2 items-center">
+        <div class="flex gap-2 items-center shrink-0">
           <div xl:hidden i-majesticons:menu-alt-line w-10 h-10 />
           <NuxtLink href="/">
             <img inline-block h-8 lg:h-10 src="/ultimate-pill.svg">
           </NuxtLink>
         </div>
-        <ul hidden xl="flex justify-between gap-2 w-75%">
+        <ul hidden xl="flex justify-between gap-2 basis-75%">
           <li>
             <Dropdown label="Продукти" :items="['Продукт1', 'Продукт2', 'Продукт3']" />
           </li>
@@ -41,7 +41,7 @@
 
       <div flex gap-8 items-center h-full>
         <InputText
-          hidden lg:block
+          hidden lg:block shrink-0
           placeholder="Търсене в целия магазин..." type="text"
           icon="ri:search-line"
         />
@@ -50,10 +50,10 @@
             <span sr-only>Търсене</span>
             <span i-ri:search-line sm="w-6 h-6" inline-block cursor-pointer />
           </div>
-          <div class="profile">
-            <div flex items-center gap-3>
-              <span sr-only xxl="not-sr-only cursor-pointer">Профил</span>
-              <span i-majesticons:user-line sm="w-6 h-6" inline-block cursor-pointer />
+          <div class="profile group xxl:hover:cursor-pointer">
+            <div class="flex items-center gap-2">
+              <span sr-only xxl="not-sr-only">Профил</span>
+              <span i-majesticons:user-line sm="w-6 h-6" inline-block group-hover:cursor-pointer />
             </div>
           </div>
           <div>
