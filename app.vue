6ed163886abb694933ec143a0f19ed148d23e3f2
IvoKara <ivo.i.karaneshev@gmail.com>
2023-01-04 23:51:22 +0200

    Resolve some Lighthouse issues and errors

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <script setup>
 useHead({
   title: 'Ultimate Pill',
+  htmlAttrs: { lang: 'bg' },
   link: [
     { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
     // { rel: 'icon', type: 'image/png', href: '/favicon.ico' },
